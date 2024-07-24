858168989008ff2ea57f23172ebeb58522b7ee85
Yassine Saindou <yassinesaindou12@gmail.com>
2024-07-25 00:00:16 +0530

    First commit

--- src/app/_components/Cabin.js
+++ src/app/_components/Cabin.js
@@ -11,8 +11,8 @@ export default function Cabin({cabin}) {
   return (
     <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
 
-      Hello 
-      {/* <div className="relative scale-[1.15] -translate-x-3">
+       
+      <div className="relative scale-[1.15] -translate-x-3">
         <Image
           fill
           className=" object-cover"
@@ -51,7 +51,7 @@ export default function Cabin({cabin}) {
             </span>
           </li>
         </ul>
-      </div> */}
+      </div>
     </div>
   );
 }
