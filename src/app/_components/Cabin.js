badc768ba04107e51d53c0e3385493b922905f5c
Yassine Saindou <yassinesaindou12@gmail.com>
2024-07-24 13:08:58 +0530

    initial commit

--- src/app/_components/Cabin.js
+++ src/app/_components/Cabin.js
@@ -6,6 +6,8 @@ import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 export default function Cabin({cabin}) {
     const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
+  
+  console.log(cabin)
   return (
     <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
       <div className="relative scale-[1.15] -translate-x-3">
