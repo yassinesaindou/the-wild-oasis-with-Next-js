c985fdfd512f9b0cff09a911c37b66fe923b479f
Yassine Saindou <yassinesaindou12@gmail.com>
2024-07-25 09:36:56 +0530

    Add static username

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,5 +1,12 @@
-export default function Page(props) {
-  return <h1 className="font-semibold text-2xl text-accent-400 mb-7">Welcome Jonas</h1>;
+import { auth } from "../_lib/auth";
+
+export default async function Page(props) {
+  
+  const session = await auth();
+
+
+  
+  return <h1 className="font-semibold text-2xl text-accent-400 mb-7">Welcome {session?.user?.name}</h1>;
 }
 export const metadata = {
   title: "Account",
